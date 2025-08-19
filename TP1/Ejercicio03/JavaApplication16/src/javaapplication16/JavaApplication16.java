1e10393ecf58afc7debc2b5d68053da433d9e62d
ezesalvatierra <ezequielsalvatierra.r@gmail.com>
2025-08-19 12:54:41 -0300

    Subiendo ejercicio 3 del TP1

---
+++ TP1/Ejercicio03/JavaApplication16/src/javaapplication16/JavaApplication16.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication16;
+
+/**
+ *
+ * @author ezequ
+ */
+public class JavaApplication16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre = "Ezequiel";
+        int edad = 20;
+        double altura = 1.78;
+        boolean estudiante = true;
+        
+        System.out.println("Estudiante: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura);
+        System.out.println("es estudiante?: " + estudiante);
+    }
+    
+}
