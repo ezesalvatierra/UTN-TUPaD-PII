3716e5c10745977675c4d75b1d65851c71667d90
ezesalvatierra <ezequielsalvatierra.r@gmail.com>
2025-08-19 13:26:01 -0300

    Subiendo el ejercicio 6

---
+++ TP1/Ejercicio06/Ejercicio06/src/ejercicio06/Ejercicio06.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio06;
+
+/**
+ *
+ * @author ezequ
+ */
+public class Ejercicio06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nombre: Juan Pérez\nEdad: 30 años\nDirección: \"Calle falsa 123\"");
+    }
+    
+}
